bfc097ab1f0dafe610025282ee87ec46b24ea05b
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-07-14 12:41:14 +0300

    login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,19 @@
-import { registerUser } from '../services/auth.js';
+import { registerUser, loginUser } from '../services/auth.js';
+
+const setupResponseSession = (
+  res,
+  { refreshToken, refreshTokenValidUntil, _id },
+) => {
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    expires: refreshTokenValidUntil,
+  });
+
+  res.cookie('sessionId', _id, {
+    httpOnly: true,
+    expires: refreshTokenValidUntil,
+  });
+};
 
 const register = async (req, res) => {
   const user = await registerUser(req.body);
@@ -10,4 +25,15 @@ const register = async (req, res) => {
   });
 };
 
-export const usersController = { register };
+const login = async (req, res) => {
+  const user = await loginUser(req.body);
+  setupResponseSession(res, user);
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: { accessToken: user.accessToken },
+  });
+};
+
+export const usersController = { register, login };
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,22 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
 import { UsersCollection } from '../db/models/users.js';
+import {
+  ACCESS_TOKEN_LIFETIME,
+  REFRESH_TOKEN_LIFETIME,
+} from '../constans/index.js';
+import { SessionCollection } from '../db/models/session.js';
+
+export const createSession = (userId) => {
+  return {
+    userId,
+    accessToken: randomBytes(30).toString('base64'),
+    refreshToken: randomBytes(30).toString('base64'),
+    accessTokenValidUntil: Date.now() + ACCESS_TOKEN_LIFETIME,
+    refreshTokenValidUntil: Date.now() + REFRESH_TOKEN_LIFETIME,
+  };
+};
 
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -17,4 +33,28 @@ export const registerUser = async (payload) => {
   });
 
   return newUser.toJSON();
+};
+
+export const loginUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+
+  if (!user) {
+    throw createHttpError(401, 'Email or passwor invalid');
+  }
+
+  const comparedPassword = await bcrypt.compare(
+    payload.password,
+    user.password,
+  );
+
+  if (!comparedPassword) {
+    throw createHttpError(401, 'Email or passwor invalid');
+  }
+
+  const session = createSession(user._id);
+
+  await SessionCollection.findOneAndDelete({ userId: user._id });
+
+  const newSession = await SessionCollection.create(session);
+  return newSession;
 };
